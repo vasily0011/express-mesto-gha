7473b14a5b9dae749790491eae434e33c1657d26
Vasily Boldyrev <vasil_001@mail.ru>
2022-10-16 01:17:44 +0300

    ошибка singup3

--- controllers/users.js
+++ controllers/users.js
@@ -46,8 +46,10 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => {
-      res.send({ user });
+    .then(() => {
+      res.send({
+        name, about, avatar, email,
+      });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
