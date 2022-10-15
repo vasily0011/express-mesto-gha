a10c2b1204a4001ebc68759a2ad7be39f39153d1
Vasily Boldyrev <vasil_001@mail.ru>
2022-10-15 18:12:52 +0300

    защищены роуты

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { notFound } = require('./errors/errors');
+const { celebrate, Joi } = require('celebrate');
+const auth = require('./middlewares/auth');
+const { createUser, login } = require('./controllers/users');
+const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 
@@ -9,21 +12,39 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '633025075134bdf85c4ef7a2',
-  };
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      password: Joi.string().required(),
+      email: Joi.string().required().email(),
+    }),
+  }),
+  login,
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(
+        /^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/,
+      ),
+      password: Joi.string().required(),
+      email: Joi.string().required().email(),
+    }),
+  }),
+  createUser,
+);
+
+app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('/', (req, res) => {
-  res.status(notFound).send({ message: 'Страница не найдена' });
-});
+app.use('/', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
