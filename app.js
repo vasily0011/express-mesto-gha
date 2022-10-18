68e29ac8bbe05d93c4671cee4cab2240ac88690c
Vasily Boldyrev <vasil_001@mail.ru>
2022-10-18 21:42:13 +0300

    вернулись в рабочее состояние

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
+mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
 app.use(cookieParser());
 app.use(bodyParser.json());
